f9bcf5a3b473ecfe7c8031ed7a076f229c4bb592
sophielem <lematre.sophiegmail.com>
2019-02-22 17:11:53 +0100

    first commit

---
+++ ts/src/index.ts
@@ -0,0 +1,53 @@
+import jsfile = require("jsonfile");
+import program = require("commander");
+
+program
+  .version('0.1.0')
+  .option('-i, --input    [chain]', 'Path to the file')
+  .parse(process.argv);
+
+
+interface Node {
+  text: string;
+  children?: Node[];
+}
+
+
+// function work(tree_data: any): [number, number] {
+//   // Si pas d'enfants, il s'agit d'une feuille
+//   if (!tree_data.hasOwnProperty("children"))
+//       return [0, 1];
+//   // [Nodes, Leaves]
+//   let sum: [number, number] = [1, 0];
+//   let rslt: [number, number];
+//   // Boucler sur les enfants
+//
+//
+//   for (let child of tree_data.children){
+//        rslt = work(child);
+//        sum = sum.map((numb, i) => numb + rslt[i]);
+//   }
+//   return sum;
+// }
+
+function work2(tree_data: any): [number, number] {
+    // Si pas d'enfants, il s'agit d'une feuille
+    if (!tree_data.hasOwnProperty("children"))
+        return [0, 1];
+
+    let res:[number, number] = [1, 0]
+    tree_data.children.map(work2).forEach((val:number, index:number)=>{
+        res[0] += val[0];
+        res[1] += val[1];
+    });
+    return res;
+}
+
+jsfile.readFile(program.input)
+    .then((tree_data: any) => {
+        let count: [number, number] = work2(tree_data);
+        console.log("Number of nodes : " + count[0] +"\nNumber of leaves : " + count[1]);
+    })
+    .catch((e) => {
+        console.log("Err " + e);
+    });
